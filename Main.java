6bb03cf8398155be9ed5b242660292cf6346fd84
pajid9763 <pajid9763@wrdsb.ca>
2020-10-11 23:07:13 +0000

    finished letter grade method

--- Main.java
+++ Main.java
@@ -1,15 +1,32 @@
 /**
- *
- * @author 
+ * method that tells the user their letter grade
+ * @author Darian
  */
 public class Main {
+  public static void examGrade(int num){
+    if (num >= 80){
+      System.out.println("A");
+    }else if (num >= 70 && num <= 79) {
+      System.out.println("B");
+    }else if (num >= 60 && num <= 69) {
+      System.out.println("C");
+    }else if (num >= 50 && num <= 59) {
+      System.out.println("D");
+    }else {
+      System.out.println("F");
+    }
+  }
 
   /**
    * The method that is executed when you hit the run button.
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    examGrade(95);
+    examGrade(74);
+    examGrade(68);
+    examGrade(55);
+    examGrade(43);
     
   }
 }
